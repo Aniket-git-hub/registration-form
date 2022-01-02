43170501569728b1d69826bf34eb3ad56cb7e9ba
Aniket Singh <singhdharmvir81@gmail.com>
2022-01-02 17:53:08 +0530

    delete the starter template

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import './../node_modules/bulma/css/bulma.css'
+require("@/assets/main.scss")
 createApp(App).use(router).mount('#app')
