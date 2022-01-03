74802a4cb7dcea5a1c5e7882e0a7d4e453e052f6
Aniket Singh <singhdharmvir81@gmail.com>
2022-01-03 17:20:32 +0530

    created a model for making new notice from admin dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -10,9 +10,6 @@ const routes = [
   {
     path: '/notice',
     name: 'notice',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/notice.vue')
   },
   {
--- src/views/admin/dashboard.vue
+++ src/views/admin/dashboard.vue
@@ -1,12 +1,70 @@
 <template>
-  <section class="section">
-      <h1 class="title is-4">Admin Dashboard</h1>
-      <h2 class="subtitle is-2"> Hello Admin</h2>
+  <section class="section is-clipped">
+    <h1 class="title is-4">Admin Dashboard</h1>
+    <h2 class="subtitle is-2">Hello Admin</h2>
+
+    <button class="button is-warning" @click="isActive = !isActive">
+      Create Notice
+    </button>
+
+    <div :class="'modal ' + (isActive ? ' is-active' : '')">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Create Notice</p>
+          <button
+            class="delete"
+            aria-label="close"
+            @click="isActive = false"
+          ></button>
+        </header>
+        <section class="modal-card-body ">
+          <form class="">
+            <div class="field">
+              <label class="label">Title</label>
+              <div class="control">
+                <input class="input" type="text" placeholder="Text input" />
+              </div>
+            </div>
+
+            <div class="field">
+              <label class="label">Priority</label>
+              <div class="control">
+                <div class="select">
+                  <select>
+                    <option>Select dropdown</option>
+                    <option>With options</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+
+            <div class="field">
+              <label class="label">Message</label>
+              <div class="control">
+                <textarea class="textarea" placeholder="Textarea"></textarea>
+              </div>
+            </div>
+
+          </form>
+        </section>
+        <footer class="modal-card-foot">
+          <button class="button is-success">Create</button>
+          <button class="button" @click="isActive = false">Cancel</button>
+        </footer>
+      </div>
+    </div>
+    
   </section>
 </template>
 
 <script>
 export default {
-name:'adminDashboard',
-}
+  name: "adminDashboard",
+  data() {
+    return {
+      isActive: false,
+    };
+  },
+};
 </script>
